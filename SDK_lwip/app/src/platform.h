ed9fcee70b53d8e4416cae1016567b15412fe99d
Marko Peshevski <markopesevski@gmail.com>
2017-01-27 03:02:40 +0100

    Mes neteja, i poso el codi a optimitzacio -03, no sembla que guanyi massa

--- SDK_lwip/app/src/platform.h
+++ SDK_lwip/app/src/platform.h
@@ -1,36 +1,13 @@
 #ifndef __PLATFORM_H_
 #define __PLATFORM_H_
 
-	#include "arch/cc.h"
-	#include "lwipopts.h"
-	#include "xenv_standalone.h"
-	#include "xparameters.h"
 	#include "xintc.h"
 	#include "xil_exception.h"
-	#include "mb_interface.h"
 	#include "xtmrctr_l.h"
 	#include "lwip/tcp.h"
-	#include "lwip/inet.h"
-	#include "lwip/ip_addr.h"
-	#include "lwip/err.h"
-	#include "lwip/tcp.h"
-	#include "xil_printf.h"
-	#include <stdio.h>
-	#include <string.h>
-
-	#define INCLUDE_RXPERF_SERVER  0
-	#define INCLUDE_TXPERF_CLIENT  0
-	#define INCLUDE_TXUPERF_CLIENT 0
-	#define INCLUDE_RXUPERF_CLIENT 0
-
-	/* timer configuration */
-	#define PLATFORM_TIMER_BASEADDR XPAR_TMRCTR_0_BASEADDR
-	#define PLATFORM_TIMER_INTERRUPT_INTR XPAR_INTC_0_TMRCTR_0_VEC_ID
-	#define PLATFORM_TIMER_INTERRUPT_MASK (1 << PLATFORM_TIMER_INTERRUPT_INTR)
-
-	#define VDD_ADC 3.3
 
 	#define PLATFORM_EMAC_BASEADDR XPAR_EMACLITE_0_BASEADDR
+	/* timer configuration */
 	#define PLATFORM_TIMER_BASEADDR XPAR_TMRCTR_0_BASEADDR
 	#define PLATFORM_TIMER_INTERRUPT_INTR XPAR_INTC_0_TMRCTR_0_VEC_ID
 	#define PLATFORM_TIMER_INTERRUPT_MASK (1 << PLATFORM_TIMER_INTERRUPT_INTR)
@@ -48,29 +25,10 @@
 	int init_platform();
 	void cleanup_platform();
 
-	void print_rxperf_app_header();
-	void print_txperf_app_header();
-	void print_utxperf_app_header();
-	void print_urxperf_app_header();
-	void start_rxperf_application();
-	int start_txperf_application();
-	void start_utxperf_application();
-	void start_urxperf_application();
-	int transfer_rxperf_data();
-	int transfer_txperf_data();
-	void transfer_utxperf_data();
-	void transfer_urxperf_data();
 	void print_ip(char *msg, struct ip_addr *ip);
 	void print_ip_settings(struct ip_addr *ip, struct ip_addr *mask, struct ip_addr *gw);
-	void print_headers();
 	void start_applications();
-	void transfer_data();
 	err_t rxperf_recv_callback(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err);
 	err_t rxperf_accept_callback(void *arg, struct tcp_pcb *newpcb, err_t err);
-
-	#if LWIP_DHCP==1
-		void dhcp_fine_tmr();
-		void dhcp_coarse_tmr();
-	#endif //LWIP_DHCP
 
 #endif //__PLATFORM_H_
