3550c49e9e465a235023f9f1773cb80f481d1f4a
Marko Peshevski <markopesevski@gmail.com>
2017-01-29 23:09:34 +0100

    Reorganitzo una mica les proves i afegeixo una mes amb Cat3

--- proves/for_loop.sh
+++ proves/for_loop.sh
@@ -2,7 +2,7 @@
 for i in `seq 20 10 1000`;
 do
 	echo $i
-	# sudo ping 192.168.1.200 -c 100 -s $i -q -i 0.01 -w 0.01 >> lwip/lwip.txt
-	# sudo ping 192.168.1.200 -c 100 -s $i -q -i 0.01 -w 0.01 >> no-lwip/no-lwip.txt
+	#sudo ping 192.168.1.200 -c 100 -s $i -q -i 0.01 -w 0.01 >> lwip/lwip.txt
+	#sudo ping 192.168.1.200 -c 100 -s $i -q -i 0.01 -w 0.01 >> no-lwip/no-lwip.txt
 	sleep 0.5
 done
