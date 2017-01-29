34db5a3a04fe7e3f3aa29aea9e573f8c886a7637
Marko Peshevski <markopesevski@gmail.com>
2017-01-29 23:39:58 +0100

    Afegides proves amb diferent metode de calcul del checksum

--- proves/for_loop.sh
+++ proves/for_loop.sh
@@ -4,5 +4,6 @@ do
 	echo $i
 	#sudo ping 192.168.1.200 -c 100 -s $i -q -i 0.01 -w 0.01 >> lwip/lwip.txt
 	#sudo ping 192.168.1.200 -c 100 -s $i -q -i 0.01 -w 0.01 >> no-lwip/no-lwip.txt
+	sudo ping 192.168.1.200 -c 100 -s $i -q -i 0.01 -w 0.01 >> checksum/lenta/lenta.txt
 	sleep 0.5
 done
